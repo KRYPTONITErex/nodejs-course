ebb6437659a942ee8658350725c868a117babff1
Lwin Phyo <lwinphyo271395@gmail.com>
2025-02-17 14:17:02 +0700

    Epi_4 commit

---
+++ module1.js
@@ -0,0 +1,9 @@
+const myName = 'Lwin Phyo';
+const myAge = 29;
+const another = 'one';
+
+module.exports = {
+    myAge,
+    myName,
+    another
+};
---
+++ module2.js
@@ -0,0 +1,8 @@
+const {myName, myAge, another} = require('./module1')
+
+// console.log(obj)
+
+// console.log(obj.myName, obj.myAge)
+
+
+console.log(myName , myAge, another);
