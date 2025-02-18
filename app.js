7822b18a68ac0f40103436fc9c710e6814b2e416
Lwin Phyo <lwinphyo271395@gmail.com>
2025-02-18 12:41:24 +0700

    Epi_14 commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', {root: __dirname});
-})  
+})
 
 app.get('/contact', (req, res) => {
     res.sendFile('./views/contact.html', {root: __dirname});
