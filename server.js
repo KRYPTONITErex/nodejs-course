062fad4031a0923021f62ed369472e03c1c01b1e
Lwin Phyo <lwinphyo271395@gmail.com>
2025-02-18 09:56:04 +0700

    Epi_7 commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("request made from client");
+    res.setHeader('Content-type', 'text/plain');
+    res.write('<h1>hello world</h1>');
+    res.end();
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log("listening on port 3000");
+});
