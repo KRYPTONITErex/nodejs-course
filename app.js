eeeff9a3254b3bd9952a5f6806c8ee0151841579
Lwin Phyo <lwinphyo271395@gmail.com>
2025-02-18 13:33:55 +0700

    Epi_16 commit

--- app.js
+++ app.js
@@ -3,31 +3,36 @@
 const express = require('express');
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
-
 
 
 app.get('/', (req, res) => {
     // res.write('<h1>HOME PAGE</h1>');
     // res.end();
     // res.send('<h1>HOME PAGE hello world</h1>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    // res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 })
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// })
 
 app.get('/contact', (req, res) => {
-    res.sendFile('./views/contact.html', {root: __dirname});
+    // res.sendFile('./views/contact.html', {root: __dirname});
+    res.render('contact');
 })
 
 app.use((req, res) => {
-    res.status(404);
-    res.sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
+    // res.sendFile('./views/404.html', {root: __dirname});
 })
